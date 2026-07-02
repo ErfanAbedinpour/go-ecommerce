e59c18ba6b63fff9fbbc382fcce389a96f6f9b72
erfan <erfan.wtf44@gmail.com>
2026-07-02 12:29:01 -0400

    refactor(domain): use i18n keys for attribute value errors

--- internal/domain/attributevalue/errors.go
+++ internal/domain/attributevalue/errors.go
@@ -1,9 +1,12 @@
 package attributevalue
 
-import "app/pkg/apperror"
+import (
+	"app/pkg/apperror"
+	"app/pkg/i18n"
+)
 
 var (
 	ErrNotFound         = apperror.NotFound("attribute value")
-	ErrValueConflict    = apperror.Conflict("attribute value already exists")
+	ErrValueConflict    = apperror.ConflictKeyed(i18n.KeyAttributeValueConflict, "attribute value already exists")
 	ErrAttributeMissing = apperror.NotFound("product attribute")
 )
