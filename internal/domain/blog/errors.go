f1f59850a43cbefed4e8255900809df52353063f
erfan <erfan.wtf44@gmail.com>
2026-07-02 12:29:01 -0400

    refactor(domain): use i18n keys for blog errors

--- internal/domain/blog/errors.go
+++ internal/domain/blog/errors.go
@@ -1,10 +1,13 @@
 package blog
 
-import "app/pkg/apperror"
+import (
+	"app/pkg/apperror"
+	"app/pkg/i18n"
+)
 
 var (
 	ErrPostNotFound     = apperror.NotFound("blog post")
 	ErrCategoryNotFound = apperror.NotFound("blog category")
 	ErrCommentNotFound  = apperror.NotFound("blog comment")
-	ErrDuplicateSlug    = apperror.Conflict("slug is already in use")
+	ErrDuplicateSlug    = apperror.ConflictKeyed(i18n.KeyBlogDuplicateSlug, "slug is already in use")
 )
