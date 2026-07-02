7eddf7dd24c13e21230871d68bc89a926d56e37c
erfan <erfan.wtf44@gmail.com>
2026-07-02 12:29:01 -0400

    chore(migrations): remove superseded migration 000015_checkout_settings.up.sql

--- migrations/000015_checkout_settings.up.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE store_settings
-    ADD COLUMN IF NOT EXISTS checkout JSONB NOT NULL DEFAULT '{
-        "min_order_toman": 100000,
-        "payment_methods": ["online", "cod"],
-        "cod_enabled": true,
-        "cod_cities": ["تهران", "کرج", "Tehran", "Karaj"],
-        "currency_label": "تومان"
-    }';
