c16846851ae92a7789a324feafc641bd48be5c21
erfan <erfan.wtf44@gmail.com>
2026-07-02 12:29:01 -0400

    chore(migrations): remove superseded migration 000008_refactor_product_skus.down.sql

--- migrations/000008_refactor_product_skus.down.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS skus;
-
-ALTER TABLE product_attributes ADD COLUMN IF NOT EXISTS value VARCHAR(200) NOT NULL DEFAULT '';
-
-ALTER TABLE products ADD COLUMN IF NOT EXISTS sku VARCHAR(100) NOT NULL DEFAULT '';
--- unique constraint may need manual cleanup on rollback if products.sku_key was dropped
