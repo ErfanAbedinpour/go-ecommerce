c60539820bb60e9d7cc886ac4f3540759993e3d9
erfan <erfan.wtf44@gmail.com>
2026-07-02 12:29:01 -0400

    chore(migrations): remove superseded migration 000012_engagement.down.sql

--- migrations/000012_engagement.down.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS product_questions;
-DROP TABLE IF EXISTS product_reviews;
-DROP TABLE IF EXISTS wishlist_items;
-DROP TABLE IF EXISTS contact_messages;
-DROP TYPE IF EXISTS product_question_status;
-DROP TYPE IF EXISTS product_review_status;
-DROP TYPE IF EXISTS contact_message_status;
-DROP TYPE IF EXISTS contact_message_source;
