ecdff6ad9ed85da7382d6691adaaa33840feb0b1
erfan <erfan.wtf44@gmail.com>
2026-07-02 12:29:01 -0400

    chore(migrations): remove superseded migration 000007_order_payment_fields.up.sql

--- migrations/000007_order_payment_fields.up.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE orders
-    ADD COLUMN payment_method  VARCHAR(50),
-    ADD COLUMN transaction_id  VARCHAR(100);
-
-CREATE INDEX idx_orders_transaction ON orders(transaction_id) WHERE transaction_id IS NOT NULL;
